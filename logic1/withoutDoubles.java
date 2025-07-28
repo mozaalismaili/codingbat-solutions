5ba89da10679dc32ba29d162fbc3d678de5d01d2
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 22:37:02 +0400

    Add solution for logic1-withoutDoubles

---
+++ logic1/withoutDoubles.java
@@ -0,0 +1,9 @@
+public class withoutDoubles{
+public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+  if (noDoubles && die1 == die2) {
+        die1 = die1 == 6 ? 1 : die1 + 1;
+    }
+    return die1 + die2;
+}
+
+}
