cbec6bf6255bc873aa87ea2f97c90a0d8e2b6133
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 22:46:31 +0400

    Add solution for logic1-shareDigit

---
+++ logic1/shareDigit.java
@@ -0,0 +1,12 @@
+public class shareDigit{
+public boolean shareDigit(int a, int b) {
+   int aLeft = a / 10;
+    int aRight = a % 10;
+    int bLeft = b / 10;
+    int bRight = b % 10;
+
+    return (aLeft == bLeft) || (aLeft == bRight) || (aRight == bLeft) || (aRight == bRight);
+
+}
+
+}
