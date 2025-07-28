f99c11fcd102188c718061c8e6d92c253efc1e83
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 15:35:00 +0400

    Add solution for warmup2-frontTimes

---
+++ warmup2/frontTimes.java
@@ -0,0 +1,18 @@
+public class frontTimes{
+public String frontTimes(String str, int n) {
+        String front;
+        if (str.length() < 3) {
+            front = str;
+        } else {
+            front = str.substring(0, 3);
+        }
+
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += front;
+        }
+        return result;
+}
+
+
+}
