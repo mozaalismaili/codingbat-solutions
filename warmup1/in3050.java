d562832bb188842dfb06b3dff52507f86ace5e9b
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 15:20:29 +0400

    Add solution for warmup1-in3050

---
+++ warmup1/in3050.java
@@ -0,0 +1,7 @@
+public class in3050{
+public boolean in3050(int a, int b) {
+  boolean in30s = (a >= 30 && a <= 40) && (b >= 30 && b <= 40);
+  boolean in40s = (a >= 40 && a <= 50) && (b >= 40 && b <= 50);
+   return in30s || in40s;
+}
+}
