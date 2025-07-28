3d4d6466143ef2a14518b50578ad271ef43c5185
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 21:55:50 +0400

    Add solution for logic1-squirrelPlay

---
+++ logic1/squirrelPlay.java
@@ -0,0 +1,10 @@
+public class squirrelPlay{
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if (isSummer) {
+        if (temp >= 60 && temp <= 100) return true;
+    } else {
+        if (temp >= 60 && temp <= 90) return true;
+    }
+    return false;
+}
+}
