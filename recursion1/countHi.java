002b9f59cab356adee91583e57969cc0fa9c0d1e
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 20:34:14 +0400

    Add solution for recursion1-countHi

---
+++ recursion1/countHi.java
@@ -0,0 +1,10 @@
+public class countHi{
+public int countHi(String str) {
+  if (str.length() < 2) return 0;
+        if (str.startsWith("hi")) {
+            return 1 + countHi(str.substring(2));
+        }
+        return countHi(str.substring(1));
+}
+
+}
