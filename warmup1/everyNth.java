a30ea174fd730f81cbdc693afc6ad4a9f5a8ea32
Moza Amur <mozaamur1602@gmail.com>
2025-07-28 15:26:07 +0400

    Add solution for warmup1-everyNth

---
+++ warmup1/everyNth.java
@@ -0,0 +1,11 @@
+public class everyNth{
+public String everyNth(String str, int n) {
+  String result = "";
+
+        for (int i = 0; i < str.length(); i += n) {
+            result += str.charAt(i);
+        }
+
+        return result;
+}
+}
