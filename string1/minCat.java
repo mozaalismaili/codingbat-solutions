35198fdfb701538504f7cabb808ff34cf127f523
Moza Amur <mozaamur1602@gmail.com>
2025-07-29 12:23:09 +0400

    Add solution for string1-minCat

---
+++ string1/minCat.java
@@ -0,0 +1,15 @@
+public class minCat{
+public String minCat(String a, String b) {
+        int lenA = a.length();
+        int lenB = b.length();
+        
+        if (lenA > lenB) {
+            a = a.substring(lenA - lenB);
+        } else if (lenB > lenA) {
+            b = b.substring(lenB - lenA);
+        }
+
+        return a + b;
+}
+
+}
