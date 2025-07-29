c2ec0075ed01c631b9753a6f058f4551e7925ac0
Moza Amur <mozaamur1602@gmail.com>
2025-07-29 12:17:03 +0400

    Add solution for string1-hasBad

---
+++ string1/hasBad.java
@@ -0,0 +1,6 @@
+public class hasBad{
+public boolean hasBad(String str) {
+ return (str.length() >= 3 && str.substring(0, 3).equals("bad")) || (str.length() >= 4 && str.substring(1, 4).equals("bad")); 
+}
+
+}
