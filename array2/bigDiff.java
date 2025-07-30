191e7d18e8c6a76ceafb44b889eaf40e900a865b
Moza Amur <mozaamur1602@gmail.com>
2025-07-30 14:58:33 +0400

    Add solution for array2-bigDiff

---
+++ array2/bigDiff.java
@@ -0,0 +1,14 @@
+public class bigDiff{
+public int bigDiff(int[] nums) {
+    int min = nums[0];
+    int max = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+        min = Math.min(min, nums[i]);
+        max = Math.max(max, nums[i]);
+    }
+
+    return max - min;
+}
+
+}
